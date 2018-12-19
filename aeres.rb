f028a2c444c6858fc0d062e62257d459c5af00ea
Jason Zhu <27667649+renqiz@users.noreply.github.com>
2018-12-19 15:25:46 +0800

    Update aeres formula

--- aeres.rb
+++ aeres.rb
@@ -1,28 +1,19 @@
 class Aeres < Formula
-  desc "Aeres binary installer"
-  homepage "https://aereslab.com"
-  url "https://github.com/aeres-io/aeres-bin/archive/v1.0.2.tar.gz"
-  sha256 "18d7ec8549e1c720e92859364928da04b1e4f5491a3281ffed54c9aff87600bc"
-
+  desc "AERES client"
+  homepage "https://www.aeres.io"
+  url "https://github.com/aeres-io/aeres-bin/raw/master/macos/x64/aeres-1.0.4.tar.xz"
+  sha256 "9c8674735ba97d0078229febc25923e68675da2ea95e7ff09c9c72f58469a263"
+  version "1.0.4"
   depends_on "openssl"
+  depends_on "readline"
   depends_on "nss"
   depends_on "nspr"
 
   def install
-    bin.install "aeres"
-    lib.install "libquic.dylib"
-  end
+    # ENV.deparallelize  # if your formula fails when building in parallel
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test aeres-bin`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    # system "tar -Jvxf aeres-1.0.4.tar.xz"
+    bin.install "bin/aeres"
+    lib.install "lib/libquic.dylib"
   end
 end
